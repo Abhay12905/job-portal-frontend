d1228ba748f606e3f9d7b4e249b1872af9b8147f
Abhay12905 <rawatabhay54321@gmail.com>
2026-05-21 10:58:37 +0530

    frontend auth fix

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { api } from "../Services/axiosConfig";
 
 function SignIn() {
   const navigate = useNavigate();
